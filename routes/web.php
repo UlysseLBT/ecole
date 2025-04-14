1a5994e280fec2cadada5a1b958a2735238424fb
UlysseLBT <lebouteiller.ulysse@gmail.com>
2025-04-14 10:26:25 +0200

    ss

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('accueil');
 });
 Route::get('/etudiants', function () {
     return view('etudiants');
-});
-Route::get('/acceuil', function () {
-    return view('acceuil');
 });
